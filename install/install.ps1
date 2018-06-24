f66ed982c706e3c4445459a0907073492c610b65
Bates Westmoreland <rbwestmoreland@gmail.com>
2018-06-24 19:27:54 -0400

    added install scripts for windows and linux

---
+++ install/install.ps1
@@ -0,0 +1,33 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Create a temporary folder for download
+$tempFolder = Join-Path $env:TEMP "git-hooks"
+New-Item $tempFolder -ItemType Directory -Force | Out-Null
+
+# Get the latest release
+$latestRelease = Invoke-WebRequest "https://api.github.com/repos/rbwestmoreland/git-hooks/releases/latest" | ConvertFrom-Json | Select-Object tag_name
+$tag = $latestRelease.tag_name
+
+# Download the zip
+Write-Host "Installing latest version ($tag)..." -NoNewline
+$client = New-Object "System.Net.WebClient"
+$url = "https://github.com/rbwestmoreland/git-hooks/releases/download/$tag/git-hooks-$tag-win-x64.zip"
+$zipFile = Join-Path $tempFolder "git-hooks.zip"
+$client.DownloadFile($url, $zipFile)
+
+# Unzip to Install Directory
+$installationFolder = Join-Path $env:ProgramData "rbwestmoreland\git-hooks"
+Remove-Item $installationFolder -Recurse -Force -ErrorAction SilentlyContinue
+Microsoft.PowerShell.Archive\Expand-Archive $zipFile -DestinationPath $installationFolder -Force
+Remove-Item $tempFolder -Recurse -Force
+
+# Update PATH
+$path = [System.Environment]::GetEnvironmentVariable("path", [System.EnvironmentVariableTarget]::User);
+$paths = $path.Split(";") -inotlike "*rbwestmoreland\git-hooks*" 
+$paths += $installationFolder
+$path = $paths -join ";"
+[System.Environment]::SetEnvironmentVariable("path", $path, [System.EnvironmentVariableTarget]::User)
+
+# Success
+Write-Host "Success." -ForegroundColor Green
+Write-Host "You PATH environment variable has been updated. Please restart any open shells." -ForegroundColor Yellow
---
+++ install/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Detect OS
+if [ "$(uname)" == "Darwin" ]; then
+    os="osx"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    os="linux"
+else
+    os="$(uname)"
+    echo "Unsupported Operating System: $os."
+    exit 1
+fi
+
+# Variables
+lib_path=/usr/local/lib/git-hooks
+bin_path=/usr/local/bin/git-hooks
+download_path=$lib_path/git-hooks.tar.gz
+
+# Get the latest release
+tag=$(curl -s https://api.github.com/repos/rbwestmoreland/git-hooks/releases/latest | grep -Eo "\"tag_name\":\s*\"(.*)\"" | cut -d'"' -f4)
+
+# Download the tar
+rm -rf $lib_path
+mkdir $lib_path
+curl -sL https://github.com/rbwestmoreland/git-hooks/releases/download/$tag/git-hooks-$tag-$os-x64.tar.gz > $download_path
+
+# Uncompress to install directory
+tar -xzf $download_path --directory=$lib_path
+rm $download_path
+
+# Make Executable
+chmod +x $lib_path/git-hooks
+ln -sfn $lib_path/git-hooks $bin_path
